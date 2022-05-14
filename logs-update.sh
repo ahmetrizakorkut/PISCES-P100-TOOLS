d010400382c6b59a949d04a3d0e797aca2ccec95
AHMET RIZA KORKUT <102479406+ahmetrizakorkut@users.noreply.github.com>
2022-05-15 00:28:27 +0300

    Update logs-update.sh

--- logs-update.sh
+++ logs-update.sh
@@ -5,10 +5,11 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echo "Updating Logs Page"
+echo "::::::::::::  Updating Logs Page  :::::::::::::::::::::::::::::::::::"
+echo ""
 cd /var/dashboard/pages/
 mv logs.php logs.php.bak
 wget https://raw.githubusercontent.com/ahmetrizakorkut/PISCES-P100-TOOLS/main/logs.php -O logs.php
-
-echo "The operation is successful. You can now refresh your Log page."
+echo ""
+echo "::::::::::: The operation is successful. You can now refresh your Log page.:::::::::::::::::::::"
 
